9a1ba434cdf08b8c87ba8659c94208d2bb36eb42
taoqf <tao_qiufeng@126.com>
2020-04-02 17:59:57 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 
-export default function awx5(param1: string, param2: string) {
+import awx2 from '@mmstudio/awx000002';
+
+export default function render(mm: awx2, data: object) {
+	return new Promise<void>((resolve) => {
+		mm.data.page.setData(data, () => {
+			resolve();
+		});
+	});
 }
